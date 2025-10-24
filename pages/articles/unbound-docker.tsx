17d055d21670c4f0c92438ecd5d8a39c0f1b641e
Owen Elliott <owenelliottdev>
2025-10-24 17:04:25 +1100

    fix code formatting

--- pages/articles/unbound-docker.tsx
+++ pages/articles/unbound-docker.tsx
@@ -93,16 +93,16 @@ RUN apk add --no-cache libevent openssl expat
 COPY --from=builder /usr/local/sbin/unbound /usr/local/sbin/unbound
 COPY --from=builder /usr/local/etc/unbound /usr/local/etc/unbound
 
-ENV UNBOUND_NUM_THREADS=8 \
-    UNBOUND_MSG_CACHE_SIZE=125m \
-    UNBOUND_RRSET_CACHE_SIZE=250m \
-    UNBOUND_PREFETCH=yes \
-    UNBOUND_DO_IP6=yes \
-    UNBOUND_EDNS_BUFFER_SIZE=1232 \
-    UNBOUND_SO_REUSEPORT=yes \
-    UNBOUND_CACHE_MAX_TTL=86400 \
-    UNBOUND_CACHE_MIN_TTL=300 \
-    UNBOUND_PREFETCH_KEY=yes \
+ENV UNBOUND_NUM_THREADS=8 \\
+    UNBOUND_MSG_CACHE_SIZE=125m \\
+    UNBOUND_RRSET_CACHE_SIZE=250m \\
+    UNBOUND_PREFETCH=yes \\
+    UNBOUND_DO_IP6=yes \\
+    UNBOUND_EDNS_BUFFER_SIZE=1232 \\
+    UNBOUND_SO_REUSEPORT=yes \\
+    UNBOUND_CACHE_MAX_TTL=86400 \\
+    UNBOUND_CACHE_MIN_TTL=300 \\
+    UNBOUND_PREFETCH_KEY=yes \\
     UNBOUND_OUTGOING_RANGE=8192 
 
 # Expose the DNS port
